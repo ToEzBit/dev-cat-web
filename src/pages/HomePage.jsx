ebe4ce316308135fd16b476c7ed5cea85cdaa755
ToEzBit <jirayut52328172@hotmail.com>
2022-06-23 17:44:22 +0700

    fix : tailwind config

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BGHome from '../asset/image/BGhome.png';
 import Cathome from '../asset/image/Cathome.png';
+import Footer from '../components/footer/Footer';
 import NavbarHome from '../components/navbars/NavbarHome';
 import Workcard from '../components/workcard/Workcard';
 
@@ -130,38 +131,7 @@ function HomePage() {
         </div>
       </div>
       {/* ================================== Footer ===================================== */}
-      <div className=" w-screen bg-gray-900 py-12">
-        <div className="max-w-screen-lg mx-auto grid grid-cols-4">
-          <div className=" text-white">LOGO</div>
-          <div>
-            <div className=" text-white">Devwork category</div>
-            <div>
-              <div className=" text-white">Mobile website</div>
-              <div className=" text-white">Desktop website</div>
-              <div className=" text-white">All website</div>
-            </div>
-          </div>
-          <div>
-            <div className=" text-white">how to use</div>
-            <div>
-              <div className=" text-white">Sign up as a developer</div>
-              <div className=" text-white">How to start selling work</div>
-              <div className=" text-white">payment of wages</div>
-              <div className=" text-white">payment of wages</div>
-              <div className=" text-white">payment of wages</div>
-            </div>
-          </div>
-          <div>
-            <div className=" text-white">contact us</div>
-            <div>
-              <div className=" text-white">hello@Devwork.co</div>
-              <div className=" text-white">Monday - Friday 9.30-22.00</div>
-              <div className=" text-white">Saturday-Sunday, public </div>
-              <div className=" text-white">holiday 10:00 a.m. - 7:00 p.m.</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,45 +4,35 @@ module.exports = {
     themes: ['light'],
   },
   theme: {
-    // screens: {
-    //   desktop: { max: '1279px' },
-    //   // => @media (max-width: 1279px) { ... }
+    extend: {
+      fontSize: {
+        '9xl': ['144px', { lineHeight: '144px' }],
+        '8xl': ['128px', { lineHeight: '128px' }],
+        '7xl': ['72px', { lineHeight: '72px' }],
+        '6xl': ['60px', { lineHeight: '60px' }],
+        '5xl': ['48px', { lineHeight: '58px', letterSpacing: '-0.04em' }],
+        '4xl': ['30px', { lineHeight: '36px' }],
+        '3xl': ['30px', { lineHeight: '36px' }],
+        '2xl': ['24px', { lineHeight: '32px' }],
+        lg: ['20px', { lineHeight: '28px' }],
+        x: ['18px', { lineHeight: '28px' }],
+        base: ['16px', { lineHeight: '24px' }],
+        xs: ['14px', { lineHeight: '16px' }],
+      },
 
-    //   laptop: { max: '1023px' },
-    //   // => @media (max-width: 1023px) { ... }
-
-    //   tablet: { max: '767px' },
-    //   // => @media (max-width: 767px) { ... }
-    // },
-    fontSize: {
-      '9xl': ['144px', { lineHeight: '144px' }],
-      '8xl': ['128px', { lineHeight: '128px' }],
-      '7xl': ['72px', { lineHeight: '72px' }],
-      '6xl': ['60px', { lineHeight: '60px' }],
-      '5xl': ['48px', { lineHeight: '58px', letterSpacing: '-0.04em' }],
-      '4xl': ['30px', { lineHeight: '36px' }],
-      '3xl': ['30px', { lineHeight: '36px' }],
-      '2xl': ['24px', { lineHeight: '32px' }],
-      lg: ['20px', { lineHeight: '28px' }],
-      x: ['18px', { lineHeight: '28px' }],
-      base: ['16px', { lineHeight: '24px' }],
-      xs: ['14px', { lineHeight: '16px' }],
+      colors: {
+        'text-placeholder': '#706D9E',
+        'text-orange': '#FF7937',
+        'text-color-footer': '#E8E7FF',
+        'text-banner': '#F0D5BB',
+        'text-btn': '#5D5FEF',
+        'text-card': '#57548A',
+        'p-detail': '#908FA6',
+        stroke: '#A5A6F',
+        link: '#9747FF',
+        star: '#F8D57E',
+      },
     },
-
-    colors: {
-      'text-placeholder': '#706D9E',
-      'text-orange': '#FF7937',
-      'text-color-footer': '#E8E7FF',
-      'text-banner': '#F0D5BB',
-      'text-btn': '#5D5FEF',
-      'text-card': '#57548A',
-      'p-detail': '#908FA6',
-      stroke: '#A5A6F',
-      link: '#9747FF',
-      star: '#F8D57E',
-    },
-
-    extend: {},
   },
   plugins: [require('daisyui')],
 };
