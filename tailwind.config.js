0022a7872c51b311ef71b0c0e1756c9e341bebf2
ntermpmm <thawin.kuwa@gmail.com>
2022-06-23 11:56:06 +0700

    chore config tailwind daysi axios react-hook-form react-router-dom yup heroicons

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  daisyui: {
+    themes: ['light'],
+  },
+  theme: {
+    fontSize: {
+      '9xl': ['144px', { lineHeight: '144px' }],
+      '8xl': ['128px', { lineHeight: '128px' }],
+      '7xl': ['72px', { lineHeight: '72px' }],
+      '6xl': ['60px', { lineHeight: '60px' }],
+      '5xl': ['48px', { lineHeight: '58px', letterSpacing: '-0.04em' }],
+      '4xl': ['30px', { lineHeight: '36px' }],
+      '3xl': ['30px', { lineHeight: '36px' }],
+      '2xl': ['24px', { lineHeight: '32px' }],
+      lg: ['20px', { lineHeight: '28px' }],
+      x: ['18px', { lineHeight: '28px' }],
+      base: ['16px', { lineHeight: '24px' }],
+      xs: ['14px', { lineHeight: '16px' }],
+    },
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+};
