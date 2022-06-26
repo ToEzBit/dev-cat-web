877a20c03a0c65c3d9e7bc7e0ed07f8cda69cc43
a.chanyanit <78626332+chanyaya@users.noreply.github.com>
2022-06-26 18:27:30 +0700

    125 review page (#8)
    * feat: add review page, and review card
    
    * feat: add ability to toggle between readonly/writeable mode
    
    * fix : remove console.log and never use import
    
    Co-authored-by: ToEzBit <jirayut52328172@hotmail.com>

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate, Routes, Route } from 'react-router-dom';
+import Reviews from '../components/Review.jsx/Review';
 import ChatRoom from '../pages/ChatRoom';
 import HomePage from '../pages/HomePage';
 
@@ -8,6 +9,7 @@ export default function Router() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/chatroom" element={<ChatRoom />} />
+      <Route path="/review" element={<Reviews />} />
     </Routes>
   );
 }
