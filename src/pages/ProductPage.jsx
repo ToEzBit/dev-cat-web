24cf887d75eb72368b5b0995eb81b559ee58e6ff
chanyaya <a.chanyanit@gmail.com>
2022-06-27 11:15:26 +0700

    test

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,42 @@
-import React from "react";
-import Navbar from "../components/navbars/Navbar";
+import React from 'react';
+import Carousel from '../components/carousel/Carousel';
+import Navbar from '../components/navbars/Navbar';
+import ProductDetails from '../components/products/ProductDetails';
 
 export default function ProductPage() {
-  return <div className="w-screen flex flex-col items-center">
-    <Navbar className="w-full"/>
-
-    <div className="w-10/12"></div>
-    {/* photo */}
-  </div>;
+  return (
+    <div className="w-screen flex flex-col items-center">
+      <div className="w-full">
+        <Navbar />
+      </div>
+      <div className="w-10/12 h-screen flex flex-col gap-2">
+        <div className="flex justify-end gap-2">
+          <button class="btn btn-outline btn-info btn-sm">Edit</button>
+          <button class="btn btn-outline btn-info btn-sm">Delete</button>
+        </div>
+        <div className="grid grid-cols-4 grid-rows-4 w-full h-3/6">
+          <div className="col-start-1 col-span-2 row-start-1 row-span-4 bg-slate-300">
+            {/* <Carousel /> */}
+          </div>
+          <div className="col-start-3 col-span-2 row-span-2 bg-fuchsia-400"></div>
+          <div className="col-start-3 row-start-3 bg-fuchsia-600"></div>
+          <div className="col-start-4 row-start-3 bg-fuchsia-200"></div>
+          <div className="col-start-3 row-start-4 bg-fuchsia-300"></div>
+          <div className="col-start-4 row-start-4 bg-fuchsia-100"></div>
+        </div>
+        <div>
+          <ProductDetails message={message} />
+        </div>
+        <div>
+          <div>
+            <p>Package</p>
+            <button class="btn btn-outline btn-info btn-sm">
+              Edit Package
+            </button>
+            <PackageDetails />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Navigate, Routes, Route } from 'react-router-dom';
 import ChatRoom from '../pages/ChatRoom';
 import HomePage from '../pages/HomePage';
+import ProductPage from '../pages/ProductPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/chatroom" element={<ChatRoom />} />
-      <Route path="/product/:id" element={<ProductPage/>} />
+      <Route path="/product/:id" element={<ProductPage />} />
     </Routes>
   );
 }
