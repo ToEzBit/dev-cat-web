d8185f22065e9a61a988e804f8ff1dadd37f0778
Earnpnp <pannaporn.cha@gmail.com>
2022-06-27 17:09:26 +0700

    fix : router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,6 +5,11 @@ import ChatRoom from '../pages/ChatRoom';
 import HomePage from '../pages/HomePage';
 import DevProfilePage from '../pages/DevProfilePage';
 import ResultPage from '../pages/ResultPage';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import ChangePasswordPage from '../pages/ChangePasswordPage';
+import ForgotPasswordPage from '../pages/ForgotPasswordPage';
+import NewPasswordPage from '../pages/NewPasswordPage';
 
 export default function Router() {
   return (
@@ -14,6 +19,11 @@ export default function Router() {
       <Route path="/chatroom" element={<ChatRoom />} />
       <Route path="/review" element={<Reviews />} />
       <Route path="/dev/profile/:id" element={<DevProfilePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
+      <Route path="/changepassword" element={<ChangePasswordPage />} />
+      <Route path="/newpassword" element={<NewPasswordPage />} />
     </Routes>
   );
 }
