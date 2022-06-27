44a327340b3ece61080b15eabfa95580decca24b
a.chanyanit <78626332+chanyaya@users.noreply.github.com>
2022-06-27 15:29:47 +0700

    102 feat: Add result page (#7)
    * feat: Add result page
    
    * chore: fix typos
    
    * style: add padding for result pages
    
    * refactor: add pagination ui to ResultPage
    
    * style: fix typos
    
    * style: fix typo
    
    * style: change function style
    
    * feat: Change duration input type to date
    
    * fix : map ,remove console.log
    
    Co-authored-by: ToEzBit <jirayut52328172@hotmail.com>

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,11 +3,13 @@ import { Navigate, Routes, Route } from 'react-router-dom';
 import Reviews from '../components/Review.jsx/Review';
 import ChatRoom from '../pages/ChatRoom';
 import HomePage from '../pages/HomePage';
+import ResultPage from '../pages/ResultPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/result" element={<ResultPage />} />
       <Route path="/chatroom" element={<ChatRoom />} />
       <Route path="/review" element={<Reviews />} />
     </Routes>
