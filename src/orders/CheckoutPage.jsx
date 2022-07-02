2adb7e7214158a2f8a0597c5bd4a86de64c8b924
chanyaya <a.chanyanit@gmail.com>
2022-07-02 17:17:47 +0700

    test

--- src/orders/CheckoutPage.jsx
+++ src/orders/CheckoutPage.jsx
@@ -1,55 +1,41 @@
-// import React from "react";
 
-// export default function checkoutPage() {
-//   return<form id="checkoutForm" method="POST" action="/charge">
-//   <input type="hidden" name="omiseToken">
-//   <input type="hidden" name="omiseSource">
-//   <button type="submit" id="checkoutButton">Checkout</button>
-// </form>
+import { useEffect, useState } from 'react';
+import { useOrder } from '../contexts/OrderContext';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { createPaymentIntent } from '../api/order';
+import CheckoutForm from './CheckoutForm';
 
-// \
-// }
+const stripePromise = loadStripe(
+  'pk_test_51LGO25J0De5S2BwxtNSp0yGpXK7bRGe475uYsBdLccGVPbf7zDtQMTSHHUBQLoGJMRphPubYQf7deuIyVtls5Dw900ZacHe3vj',
+);
 
+export default function CheckoutPage() {
+  const [clientSecret, setClientSecret] = useState(null);
 
+  useEffect(() => {
+    const getClientSecret = async (items = 1) => {
+      const res = await createPaymentIntent(items);
+      setClientSecret(res);
+    };
+    getClientSecret();
+  }, []);
 
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
 
-// import { useEffect, useState } from 'react';
-// import { useOrder } from '../contexts/OrderContext';
-// import { Elements } from '@stripe/react-stripe-js';
-// import { loadStripe } from '@stripe/stripe-js';
-// import { createPaymentIntent } from '../api/order';
-// import CheckoutForm from './CheckoutForm';
-
-// const stripePromise = loadStripe(
-//   'pk_test_51LGO25J0De5S2BwxtNSp0yGpXK7bRGe475uYsBdLccGVPbf7zDtQMTSHHUBQLoGJMRphPubYQf7deuIyVtls5Dw900ZacHe3vj',
-// );
-
-// export default function CheckoutPage() {
-//   const [clientSecret, setClientSecret] = useState(null);
-
-//   useEffect(() => {
-//     const getClientSecret = async (items = 1) => {
-//       const res = await createPaymentIntent(items);
-//       setClientSecret(res);
-//     };
-//     getClientSecret();
-//   }, []);
-
-//   const appearance = {
-//     theme: 'stripe',
-//   };
-//   const options = {
-//     clientSecret,
-//     appearance,
-//   };
-
-//   return (
-//     <div>
-//       {clientSecret && (
-//         <Elements options={options} stripe={stripePromise}>
-//           <CheckoutForm />
-//         </Elements>
-//       )}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
