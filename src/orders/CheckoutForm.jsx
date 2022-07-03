db4c37b6ef181594f6ca0019e1db2f512b4b4e87
chanyaya <a.chanyanit@gmail.com>
2022-07-03 23:26:25 +0700

    feat: completed checkout page and form

--- src/orders/CheckoutForm.jsx
+++ src/orders/CheckoutForm.jsx
@@ -4,18 +4,26 @@ import {
   useStripe,
   useElements,
 } from '@stripe/react-stripe-js';
+import { orderPayment } from '../../api/order';
+import { useNavigate } from 'react-router-dom';
 
-export default function CheckoutForm() {
+export default function CheckoutForm({ orderId = 2 }) {
+  const navigate = useNavigate();
   const stripe = useStripe();
   const elements = useElements();
 
   const [message, setMessage] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+
+  const paymentConfirmed = async (id) => {
+    // const res = await orderPayment({ transactionId: id }, orderId);
+    navigate('/');
+  };
 
   useEffect(() => {
     if (!stripe) {
       return;
     }
+
     const clientSecret = new URLSearchParams(window.location.search).get(
       'payment_intent_client_secret',
     );
@@ -24,68 +32,84 @@ export default function CheckoutForm() {
       return;
     }
 
-    const test = async () => {
-      const res = await stripe.retrievePaymentIntent(clientSecret);
-    };
-    test()
-
     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
       switch (paymentIntent.status) {
         case 'succeeded':
-          setMessage('Payment succeeded!');
+          paymentConfirmed(paymentIntent.id);
+          setMessage(`Payment succeeded!, redirecting to homepage...`);
           break;
         case 'processing':
           setMessage('Your payment is processing.');
+          console.log(message);
           break;
         case 'requires_payment_method':
           setMessage('Your payment was not successful, please try again.');
+          console.log(message);
           break;
         default:
           setMessage('Something went wrong.');
+          console.log(message);
           break;
       }
     });
   }, [stripe]);
 
+  const clientSecret = new URLSearchParams(window.location.search).get(
+    'payment_intent_client_secret',
+  );
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (!stripe || !elements) {
       return;
     }
 
-    setIsLoading(true);
-
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        //payment completion page
-        return_url: 'http://localhost:3000',
+        // redirect ไปหน้านี้ถ้าข้อมูลบัตรถูกต้อง
+        return_url: `${window.location.href}`,
       },
     });
+
     if (error.type === 'card_error' || error.type === 'validation_error') {
       setMessage(error.message);
     } else {
       setMessage('An unexpected error occurred.');
     }
-
-    setIsLoading(false);
   };
+
   return (
-    <div>
-      <form id="payment-form" onSubmit={handleSubmit}>
-        <PaymentElement id="payment-element" />
-        <button disabled={isLoading || !stripe || !elements} id="submit">
-          <span id="button-text">
-            {isLoading ? (
-              <div className="spinner" id="spinner"></div>
-            ) : (
-              'Pay now'
-            )}
-          </span>
-        </button>
-        {/* Show any error or success messages */}
-        {message && <div id="payment-message">{message}</div>}
-      </form>
-    </div>
+    <form
+      id="payment-form"
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 justify-center"
+    >
+      <PaymentElement id="payment-element" />
+      <button
+        disabled={!stripe || !elements}
+        id="submit"
+        className="btn btn-md btn-info rounded-md"
+      >
+        <span id="button-text">Pay Now</span>
+      </button>
+      {message && (
+        <div id="payment-message" className="mx-auto">
+          {message}
+        </div>
+      )}
+    </form>
   );
 }
+
+//use the following card number for testing
+// 1. 4242424242424242 : successful payment(credit card)
+// 2. 4000056655665556 : successful payment(debit card)
+// 3. 4000000000000002 : generic decline
+// 4. 4000000000009995 : insufficient fund decline
+// 5. 4000000000009979 : stolen card decline
+// 6. 4000000000000127 : incorrect ccv decline
+// 7. 4000000000000119 : processing error decline
+// 8. 4242424242424241 : incorrect number decline
+//expiration date/cvc can be anything
---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { createPaymentIntent } from '../api';
+
+import CheckoutForm from '../orders/CheckoutForm';
+
+// devCat's publishable key
+const stripePromise = loadStripe(
+  'pk_test_51LGO25J0De5S2BwxtNSp0yGpXK7bRGe475uYsBdLccGVPbf7zDtQMTSHHUBQLoGJMRphPubYQf7deuIyVtls5Dw900ZacHe3vj',
+);
+
+export default function CheckoutPage({ productId }) {
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    // from chatroom > click pay now > load this page and run this function
+    // Create PaymentIntent as soon as the page loads
+    const getClientSecret = async () => {
+      const res = await createPaymentIntent(productId);
+      setClientSecret(res?.data?.clientSecret);
+    };
+    getClientSecret();
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="max-w-screen min-w-screen flex justify-center h-screen items-center">
+      <div className="w-1/2 h-1/2">
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise} key={clientSecret}>
+            <CheckoutForm clientSecret={clientSecret} />
+          </Elements>
+        )}
+      </div>
+    </div>
+  );
+}
