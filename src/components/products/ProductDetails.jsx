5f69315d8798d73bae2cf97435efa877a1b9badd
ToEzBit <jirayut52328172@hotmail.com>
2022-07-04 23:26:32 +0700

    fix :  product info

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function ProductDetails({ message }) {
-  return <div>{message}</div>;
+  return (
+    <div className="prose text-left  max-w-none">
+      <td dangerouslySetInnerHTML={{ __html: message }}></td>
+    </div>
+  );
 }
 
 export default ProductDetails;
