691528f65413cd4125ea1263d533ab9310c3cf4d
ToEzBit <jirayut52328172@hotmail.com>
2022-07-05 20:04:44 +0700

    fix : package info,dev avatar profile

--- src/components/products/PackageDetails.jsx
+++ src/components/products/PackageDetails.jsx
@@ -4,71 +4,65 @@ import { useNavigate } from 'react-router-dom';
 function PackageDetails({ product }) {
   let navigate = useNavigate();
   return (
-    <table className="table w-full text-center">
-      <colgroup>
-        {product?.map((element, idx) => (
-          <col key={idx} className={element.title}></col>
-        ))}
-      </colgroup>
-      <thead>
-        <tr>
-          <td></td>
+    <div className="overflow-x-scroll">
+      <table className="table  text-center">
+        <colgroup>
           {product?.map((element, idx) => (
-            <th scope="col" key={idx}>
-              <h6>{element.title}</h6>
-              {element.info}
-            </th>
+            <col key={idx} className={element.title}></col>
           ))}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">revision</th>
-          {product?.map((element, idx) => (
-            <td key={idx}>{element.revision}</td>
-          ))}
-        </tr>
-        <tr>
-          <th scope="row">duration</th>
-          {product?.map((element, idx) => (
-            <td key={idx}>{element.duration} days</td>
-          ))}
-        </tr>
+        </colgroup>
+        <thead className="">
+          <tr>
+            <td></td>
+            {product?.map((element, idx) => (
+              <th scope="col" key={idx}>
+                <h6>{element.title}</h6>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">revision</th>
+            {product?.map((element, idx) => (
+              <td key={idx}>{element.revision}</td>
+            ))}
+          </tr>
+          <tr>
+            <th scope="row">duration</th>
+            {product?.map((element, idx) => (
+              <td key={idx}>{element.duration} days</td>
+            ))}
+          </tr>
 
-        <tr>
-          <th scope="row">Additional details</th>
+          <tr>
+            <th scope="row">Additional details</th>
+            {product?.map((element, idx) => (
+              <td key={idx}>
+                <p className="whitespace-normal  break-words">{element.info}</p>
+              </td>
+            ))}
+          </tr>
 
-          {product?.map((element, index) => (
-            <td key={index}>
-              <ul className="flex flex-col justify-end">
-                {element.PackageDetails.map((el, idx) => (
-                  <li key={idx}>
-                    {el.title}: {el.value}
-                  </li>
-                ))}
-              </ul>
-            </td>
-          ))}
-        </tr>
-
-        <tr>
-          <th scope="row">price</th>
-          {product?.map((element, idx) => (
-            <td key={idx}>
-              <div className="flex flex-col items-center gap-2">
-                <div>{element.price} THB </div>
-                <button
-                  className="btn btn-info btn-xs w-1/2"
-                  onClick={() => navigate('/chatroom')}
-                >
-                  สนใจจ้าง
-                </button>
-              </div>
-            </td>
-          ))}
-        </tr>
-      </tbody>
-    </table>
+          <tr>
+            <th scope="row">price</th>
+            {product?.map((element, idx) => (
+              <td key={idx}>
+                <div className="flex flex-col items-center gap-2">
+                  <div>{element.price} THB </div>
+                  <button
+                    className="btn btn-info btn-xs w-1/2"
+                    onClick={() => navigate('/chatroom')}
+                  >
+                    สนใจจ้าง
+                  </button>
+                </div>
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
