fcb49591d3311541b6ae53b401bf59707f8d68db
ToEzBit <jirayut52328172@hotmail.com>
2022-07-06 00:30:30 +0700

    feat : add Link to dev profile

--- src/components/card/WorkCard.jsx
+++ src/components/card/WorkCard.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ProfilePic from '../../asset/image/ProfilePic.png';
 
-function WorkCard({ workcard }) {
+function WorkCard({ workcard, devId }) {
   const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   };
-  console.log(workcard);
-  // if (workcard.Packages) {
-  //   var packageArr = workcard.Packages;
-  //   var minPriceOfPackage = packageArr.reduce((prev, curr) =>
-  //     +prev.price < +curr.price ? prev : curr,
-  //   );
-  // }
-
+  let minPriceOfPackage;
+  if (workcard.Packages) {
+    const packageArr = workcard.Packages;
+    const findMinPriceOfPackage = packageArr.reduce((prev, curr) =>
+      +prev.price < +curr.price ? prev : curr,
+    );
+    minPriceOfPackage = numberWithCommas(findMinPriceOfPackage.price);
+  } else {
+    minPriceOfPackage = numberWithCommas(workcard.minPrice);
+  }
   return (
-    <div>
-      <div className="hover:-translate-y-1 hover:scale-110 duration-300 group">
-        <div className="card w-18 bg-base-100 shadow-xl max-w-xs ">
-          <div className="flex justify-between px-4 py-4 pb-4 items-center">
-            <div className="flex gap-2">
+    <div className="hover:-translate-y-1 hover:scale-110 duration-300 group">
+      <div className="card w-18 bg-base-100 shadow-xl max-w-xs ">
+        <div className="flex justify-between px-4 py-4 pb-4 items-center">
+          <div className="flex gap-2">
+            <Link to={`/dev/profile/${workcard.Dev.id}`}>
               <img
                 className="w-12 rounded-full h-12 object-cover"
                 src={workcard?.Dev.profileImage}
                 alt=""
               />
-              <h2 className="card-title">{workcard?.Dev.username}</h2>
-            </div>
-            <div className="badge border-none bg-amber-300 text-gray-600">
-              {workcard?.avgReview}.0
-            </div>
+            </Link>
+            <h2 className="card-title">{workcard?.Dev.username}</h2>
           </div>
+          <div className="badge border-none bg-amber-300 text-gray-600">
+            {workcard?.avgReview}.0
+          </div>
+        </div>
+        <Link to={`/product/${workcard.id}`}>
           <figure className="h-52 w-full">
             {workcard.ProductImages ? (
               <>
@@ -47,13 +52,13 @@ function WorkCard({ workcard }) {
               </>
             )}
           </figure>
-          <div className="p-4 pt-8 -mt-4">
-            <p className="">{workcard?.title}</p>
-            <div className=" flex justify-between items-center pt-4">
-              <div className="badge badge-outline">{workcard?.category}</div>
-              <div className="badge badge-accent text-white">
-                {/* {numberWithCommas(workcard?.minPrice)}B */}
-              </div>
+        </Link>
+        <div className="p-4 pt-8 -mt-4">
+          <p className="">{workcard?.title}</p>
+          <div className=" flex justify-between items-center pt-4">
+            <div className="badge badge-outline">{workcard?.category}</div>
+            <div className="badge badge-accent text-white">
+              <p>{minPriceOfPackage} B</p>
             </div>
           </div>
         </div>
