11c10510d263475752e7e798a58d5259e3781a8b
ThawinKu <100593751+ntermpmm@users.noreply.github.com>
2022-07-08 01:08:11 +0700

    127 style(create-product.jsx) redesign create product page  (#64)
    * style pagination change layout to 1024px
    
    * style add motion all page
    
    * style add animation
    
    * style update navbar
    
    * style update navbar
    
    * style update navbar
    
    * style update navbar
    
    * create product

---
+++ src/components/modal/ConfirmCreateProduct.jsx
@@ -0,0 +1,88 @@
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
+
+export default function ConfirmCreateProduct() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={openModal}
+        className=" bg-chat  z-10 btn btn-primary  border-none hover:bg-chat-quotation duration-500 "
+      >
+        Confirm to Create
+      </button>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full flex flex-col gap-4 max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg leading-6 text-chat font-semibold"
+                  >
+                    Please confirm
+                  </Dialog.Title>
+                  <div className="">
+                    <p className="text-sm text-text-placeholder">
+                      To add your product, please confirm that your product
+                      details has been checked, it will add to your work
+                      collection.
+                    </p>
+                  </div>
+
+                  <div className=" flex justify-center gap-8">
+                    <button
+                      type="button"
+                      className="border cursor-pointer border-stroke p-2 rounded-xl px-4 hover:bg-chat  hover:text-white duration-300  text-chat"
+                      onClick={closeModal}
+                    >
+                      Yes! I'm sure
+                    </button>
+                    <button
+                      type="button"
+                      className="border border-stroke p-2 rounded-xl px-4  hover:bg-chat  hover:text-white duration-300  text-chat"
+                      onClick={closeModal}
+                    >
+                      No I'm not
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
--- src/components/step/Step1.jsx
+++ src/components/step/Step1.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { Fragment, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+import step1 from '../../asset/video/step1.mp4';
+import { motion } from 'framer-motion';
 
 const people = [{ name: 'Web Application' }, { name: 'Mobile Application' }];
 
@@ -17,92 +19,117 @@ function Step1() {
 
   return (
     <>
-      <div className="card w-3/4 h-96 flex flex-col bg-base-100 shadow-xl mx-auto mt-20">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="card  max-w-screen-xl h-full p-20 border flex flex-col bg-base-100 shadow-xl mx-auto mt-20"
+      >
         {/*====================== Title Project ====================== */}
-        <div className="flex flex-col gap-2 ml-12 mt-6">
-          <div className="gap-5">
-            <label
-              className="block text-purple-500 text-4xl font-extrabold mb-2"
-              htmlFor="titleProject"
-            >
-              STEP 1
-            </label>
-          </div>
-          <div className="flex flex-col">
-            <label
-              className="block text-[#06033A] text-sm font-bold mb-2"
-              htmlFor="titleProject"
-            >
-              Choose Category
-            </label>
-          </div>
-          <div className="w-48">
-            <Listbox value={selected} onChange={setSelected}>
-              <div className="relative mt-1">
-                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                  <span className="block truncate">{selected.name}</span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <SelectorIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </Listbox.Button>
-                <Transition
-                  as={Fragment}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
+        <div className=" grid grid-cols-2">
+          <div className="flex flex-col gap-8">
+            <div className=" flex w-[100%] flex-col gap-4">
+              <label
+                className="block text-chat text-5xl font-extrabold"
+                htmlFor="titleProject"
+              >
+                STEP 1
+              </label>
+              <div className="flex flex-col gap-2">
+                <label
+                  className="block text-text-normal text-base font-bold"
+                  htmlFor="titleProject"
                 >
-                  <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {people.map((person, personIdx) => (
-                      <Listbox.Option
-                        key={personIdx}
-                        className={({ active }) =>
-                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                            active
-                              ? 'bg-amber-100 text-amber-900'
-                              : 'text-gray-900'
-                          }`
-                        }
-                        value={person}
-                      >
-                        {({ selected }) => (
-                          <>
-                            <span
-                              className={`block truncate ${
-                                selected ? 'font-medium' : 'font-normal'
-                              }`}
-                            >
-                              {person.name}
-                            </span>
-                            {selected ? (
-                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </Transition>
+                  Choose Category
+                </label>
+                <div className="text-sm text-text-normal opacity-60">
+                  Choose a primary category that describes your business as a
+                  whole, and be specific. When you start to enter your category,
+                  choose a category from a dropdown that shows up.
+                </div>
               </div>
-            </Listbox>
-          </div>
+            </div>
+            <div className="w-48">
+              <Listbox value={selected} onChange={setSelected}>
+                <div className="relative mt-1">
+                  <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block truncate">{selected.name}</span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <SelectorIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {people.map((person, personIdx) => (
+                        <Listbox.Option
+                          key={personIdx}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active
+                                ? 'bg-chat text-white rounded-lg'
+                                : 'text-gray-900'
+                            }`
+                          }
+                          value={person}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? 'font-medium' : 'font-normal'
+                                }`}
+                              >
+                                {person.name}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-chat ">
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+            </div>
 
-          <div className="flex flex-col gap-2">
-            <div className="card-actions">
-              <button className="btn btn-primary" onClick={handleContinue2}>
-                Continue
-              </button>
+            <div className="flex flex-col gap-2">
+              <div className="card-actions">
+                <button
+                  className="btn btn-primary bg-chat border-none hover:bg-chat-quotation duration-500"
+                  onClick={handleContinue2}
+                >
+                  Continue
+                </button>
+              </div>
             </div>
           </div>
+
+          <div className=" rounded-xl w-[100%] h-80">
+            <video
+              autoPlay
+              loop
+              muted
+              className=" object-cover h-80 rounded-xl w-full"
+              src={step1}
+            />
+          </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
