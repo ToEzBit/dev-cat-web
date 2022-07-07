e08b725e700eb45fc46d50a64f45be762b7e8c3d
ntermpmm <thawin.kuwa@gmail.com>
2022-07-08 05:12:36 +0700

    style chatroom

--- src/components/chat/step/Step.jsx
+++ src/components/chat/step/Step.jsx
@@ -31,22 +31,32 @@ export default function Step({ order, currentValue, setCurrentValue }) {
       <ul className="steps steps-vertical">
         <li className={setColorPrimary(2)}>
           <div className="flex flex-col items-start justify-start">
-            <h5>Order created</h5>
-            {order?.createdAt && <p>{formatTime(order?.createdAt)}</p>}
+            <h6 className=" font-semibold">Order created</h6>
+            {order?.createdAt && (
+              <p className=" text-xs opacity-80">
+                {formatTime(order?.createdAt)}
+              </p>
+            )}
           </div>
         </li>
         <li className={setColorPrimary(3)}>
           <div className="flex flex-col items-start justify-start">
-            <h5>Order paid</h5>
+            <h6 className=" font-semibold">Order paid</h6>
             {order?.ProofPayments?.length > 0 && (
-              <p>{formatTime(order?.ProofPayments[0].createdAt)}</p>
+              <p className=" text-xs opacity-80">
+                {formatTime(order?.ProofPayments[0].createdAt)}
+              </p>
             )}
           </div>
         </li>
         <li className={setColorPrimary(4)}>
           <div className="flex flex-col items-start justify-start">
-            <h5> Order on process</h5>
-            {order?.startDate && <p>{formatTime(order?.startDate)}</p>}
+            <h6 className=" font-semibold"> Order on process</h6>
+            {order?.startDate && (
+              <p className=" text-xs opacity-80">
+                {formatTime(order?.startDate)}
+              </p>
+            )}
           </div>
         </li>
         {order?.OrderReviews &&
@@ -54,9 +64,9 @@ export default function Step({ order, currentValue, setCurrentValue }) {
             return (
               <li className={setColorPrimary(4)} value="3" key={idx}>
                 <div>
-                  <h5>
+                  <h6 className=" font-semibold">
                     revision: {el?.countRevision}/{order?.totalRevision}
-                  </h5>
+                  </h6>
                   {/* <p className="col-start-2">{formatTime(el.createdAt)}</p> */}
                 </div>
               </li>
@@ -65,13 +75,19 @@ export default function Step({ order, currentValue, setCurrentValue }) {
         <li className={setColorPrimary(5)}>
           {order?.status === 'complete' ? (
             <div className="flex flex-col items-start justify-start">
-              <h5>Order completed</h5>
-              <p>{formatTime(order?.updatedAt)}</p>
+              <h6 className=" font-semibold">Order completed</h6>
+              <p className=" text-xs opacity-80">
+                {formatTime(order?.updatedAt)}
+              </p>
             </div>
           ) : (
             <div className="flex flex-col items-start justify-start">
-              <h5>Deadline</h5>
-              {order?.startDate && <p>{formatTime(order?.endDate)}</p>}
+              <h6 className=" font-semibold">Deadline</h6>
+              {order?.startDate && (
+                <p className=" text-xs opacity-80">
+                  {formatTime(order?.endDate)}
+                </p>
+              )}
             </div>
           )}
         </li>
