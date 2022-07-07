9f8b59ea9d062e5401ceb5e406faee82891db6c7
Earnpnp <pannaporn.cha@gmail.com>
2022-07-07 15:44:42 +0700

    fix: ui create-product&login/logout

--- src/components/step/Step5.jsx
+++ src/components/step/Step5.jsx
@@ -20,6 +20,23 @@ function Step5() {
             >
               STEP 5
             </label>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-20 w-20 mx-auto"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <p className="text-green-400 font-bold card-title text-4xl">
+              SUCCESSFULLY
+            </p>
           </div>
         </div>
         <div className="flex gap-2">
