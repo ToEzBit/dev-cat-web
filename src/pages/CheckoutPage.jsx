4ab965134ddd120dd1613bbaffd26f7d16d66220
chanyaya <a.chanyanit@gmail.com>
2022-07-08 14:09:34 +0700

    style: css checkout page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -8,6 +8,8 @@ import { useOrder } from '../contexts/OrderContext';
 import { useAuth } from '../contexts/AuthContext';
 import { useParams } from 'react-router-dom';
 
+import Alltype2 from '../../src/asset/image/Alltype2.png';
+
 // devCat's publishable key
 const stripePromise = loadStripe(
   'pk_test_51LGO25J0De5S2BwxtNSp0yGpXK7bRGe475uYsBdLccGVPbf7zDtQMTSHHUBQLoGJMRphPubYQf7deuIyVtls5Dw900ZacHe3vj',
@@ -36,8 +38,15 @@ export default function CheckoutPage() {
   };
 
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="w-1/2 h-1/2">
+    <div className="w-full h-screen flex justify-center items-center bg-stroke">
+      <div className="w-3/5 h-4/5 flex justify-evenly gap-4 items-center">
+        <img src={Alltype2} alt="" className="object-cover" />
+      </div>
+
+      <div
+        className=" bg-white flex flex-col p-4  rounded-lg "
+        style={{ margin: '0 0 0 20' }}
+      >
         {clientSecret && (
           <Elements options={options} stripe={stripePromise} key={clientSecret}>
             <CheckoutForm clientSecret={clientSecret} orderId={orderId} />
